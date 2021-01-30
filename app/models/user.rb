553788129ebef95eaf24ff7aa126d3e41a11e76b
yoshi0422 <malasada.b.k@gmail.com>
2021-01-30 17:51:09 +0900

    deviseに関するビューファイルの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :familyname, presence: true
+  validates :firstname, presence: true
+  validates :familyname_kana, presence: true
+  validates :firstname_kana, presence: true
+  validates :birthday, presence: true
 end
