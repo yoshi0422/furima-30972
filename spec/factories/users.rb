b6956e2587f4e74554d771b8c26af34acc7cc272
yoshi0422 <malasada.b.k@gmail.com>
2021-02-01 21:25:06 +0900

    モデルの単体テストコードの実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person{ Gimei.name }
+    end
+    nickname                {Faker::Name.name}
+    email                   {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password                {password}
+    password_confirmation   {password}
+    familyname              {person.last.kanji}
+    firstname               {person.first.kanji}
+    familyname_kana         {person.last.katakana}
+    firstname_kana          {person.first.katakana}
+    birthday                { Faker::Date.backward}
+  end
+end
