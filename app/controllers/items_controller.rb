d1522a21062e4a93f60a4caf17d68b5945dfac46
yoshi0422 <malasada.b.k@gmail.com>
2021-02-10 20:27:37 +0900

    販売手数料と販売利益の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authnticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @items = Item.all
   end
