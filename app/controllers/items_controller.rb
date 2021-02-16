643fcbc803bd3e89922d16976471bef2c8eaa4ea
yoshi0422 <malasada.b.k@gmail.com>
2021-02-16 18:33:22 +0900

    before_actionの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,15 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit, :update]
+  
   def index
     @items = Item.all.order("created_at DESC")
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -14,14 +18,14 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
   def show
-    @item = Item.find(params[:id])
   end
+
   def edit
-    @item = Item.find(params[:id])
   end
+  
   def update
-    item = Item.find(params[:id])
     if item.update(item_params)
       redirect_to root_path
     else
@@ -33,5 +37,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :text, :price, :image, :category_id, :condition_id, :delivery_fee_id, :prefectures_id, :delivery_days_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
