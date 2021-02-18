4ad5e926ac58c20e0c07d3d8e960172843e037cb
yoshi0422 <malasada.b.k@gmail.com>
2021-02-18 19:40:55 +0900

    ordersテーブルの実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  root to:"order#index"
+  resources :orders, only:[:create]
 end
