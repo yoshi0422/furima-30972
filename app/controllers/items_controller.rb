0be821e760efaa7acfe4441ec0b540edc5ccec79
yoshi0422 <malasada.b.k@gmail.com>
2021-02-18 12:47:33 +0900

    コントローラーとビューの修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,14 +29,11 @@ class ItemsController < ApplicationController
   end
   
   def update
-      if current_user.id == @item.user_id
-        if @item.update(item_params)
-          redirect_to item_path
-        else
-          render :edit
-        end
+    return redirect_to root_path if current_user.id != @item.user_id
+      if @item.update(item_params)
+        redirect_to item_path
       else
-        redirect_to root_path
+        render :edit
       end
   end
 
