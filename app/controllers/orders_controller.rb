01ead7392bee865f8c04cb50d1a8b46550e2734d
yoshi0422 <malasada.b.k@gmail.com>
2021-03-01 20:40:06 +0900

    createアクションの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,5 +5,12 @@ class OrdersController < ApplicationController
   end
 
   def create
+    Order.create(order_params)
+  end
+
+  private
+
+  def order_params
+    params.requre(:order).permit(:number, :exp_month, :exp_year, :cvc, :postal_code, :prefectures_id, :manicipality, :address, :building_name, :phone_number)
   end
 end
